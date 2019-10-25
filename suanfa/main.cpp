5acaa53d6041974e4a2d00ab9f11c553f4121a97
edouble <1171574037@qq.com>
2019-10-25 16:52:30 +0800

    Initial Commit

---
+++ suanfa/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  suanfa
+//
+//  Created by 张伟 on 2019/10/25.
+//  Copyright © 2019 张伟. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
